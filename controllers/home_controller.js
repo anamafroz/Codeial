e2ba71066b689e9cfa270f2b50d64dd407fc0b97
Anam Afroz <anam.anamafroz.afroz@gmail.com>
2020-11-10 20:37:25 +0530

    Implementing partials

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,9 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up for codeial</h1>');
+    // return res.end('<h1>Express is up for codeial</h1>');
+
+    return res.render('home',{
+        title:"home"
+    });
+
 }
 
