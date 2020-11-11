2257220a98e1b0a970b18a06e0e02f6c53c03fee
Anam Afroz <anam.anamafroz.afroz@gmail.com>
2020-11-11 15:41:51 +0530

    Linking our MongoDb using Mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Codeial_Development');
+
+
+const db = mongoose.connection;
+
+db.on('error',console.log.bind(console,"Error connecting to MongoDB"));
+
+db.once('open',function(){
+    console.log('Connected to  Database :: MongoDB');
+});
+
+module.exports = db;
