cc4d0d718168d0ab02f0f6dc2ddc16ff6ede3dab
Anam Afroz <anam.anamafroz.afroz@gmail.com>
2020-11-18 00:26:05 +0530

    Check Authentication on Creating Post

--- routes/post.js
+++ routes/post.js
@@ -4,10 +4,11 @@ var bodyParser = require('body-parser');
 
 var jsonParser = bodyParser.json();
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/post_controller');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.post('/create',urlencodedParser,postsController.create);
+router.post('/create',passport.checkAuthentication,urlencodedParser,postsController.create);
 
 module.exports = router;
